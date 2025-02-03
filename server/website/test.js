efb69560797bbcc6b5ccdd86344b2af10a125eac
Daniele Muffato <danixgithub1@gmail.com>
2025-02-03 17:25:11 +0100

    updated js

--- server/website/test.js
+++ server/website/test.js
@@ -1,11 +1,29 @@
-const data = new FormData()
-data.append(1, 2)
-data.append(3, 4)
+// SEND DATA AS FORM PARAMS
+//const data = new FormData()
+//data.append(1, 2)
+//data.append(3, 4)
+//
+//document.querySelector("button").addEventListener("click", function(event) {
+//    fetch("", {
+//        method: "POST",
+//        body: data
+//    })
+//        .then(res => res.text())
+//        .then(res => console.log(res))
+//})
+//
 
+// SEND DATA AS JSON
 document.querySelector("button").addEventListener("click", function(event) {
     fetch("", {
         method: "POST",
-        body: data
+        body: JSON.stringify({
+            "12": 12,
+            34: "tre",
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
     })
         .then(res => res.text())
         .then(res => console.log(res))
