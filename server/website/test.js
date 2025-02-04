c7ab5b47a3aaf9adb84aee9f17e46e7783278c52
Daniele Muffato <danixgithub1@gmail.com>
2025-02-04 16:52:03 +0100

    better tables

--- server/website/test.js
+++ server/website/test.js
@@ -13,19 +13,58 @@
 //})
 //
 
+function buildTableFromJSON(json) {
+    if (!json || !json.res || !json.res.query || !json.res.attributes) {
+        console.error("Invalid JSON structure");
+        return "";
+    }
+
+    const attributes = json.res.attributes;
+    const data = json.res.query;
+
+    // Create the table element
+    let table = "<table border='1'><thead><tr>";
+
+    // Add table headers
+    attributes.forEach(attr => {
+        table += `<th>${attr}</th>`;
+    });
+    table += "</tr></thead><tbody>";
+
+    // Add table rows
+    data.forEach(row => {
+        table += "<tr>";
+        row.forEach(cell => {
+            table += `<td>${cell}</td>`;
+        });
+        table += "</tr>";
+    });
+
+    table += "</tbody></table>";
+    return table;
+}
+
+
+
 // SEND DATA AS JSON
-document.querySelector("button").addEventListener("click", function(event) {
+function populate(table) {
     fetch("", {
         method: "POST",
         body: JSON.stringify({
-            "type": "select-city",
+            "type": `select-${table}`,
             "data": {},
         }),
         headers: {
             'Content-Type': 'application/json'
         },
     })
-        .then(res => res.text())
-        .then(res => console.log(res))
-})
+        .then(res => res.json())
+        .then(res => document.querySelector(`div#${table}`).innerHTML = buildTableFromJSON(res))
+}
 
+populate("city")
+populate("shop")
+populate("item")
+populate("detail")
+populate("payment")
+populate("method")
