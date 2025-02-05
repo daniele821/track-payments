3f1a4a710548941b91d15f2f341ca30665346ea2
Daniele Muffato <danixgithub1@gmail.com>
2025-02-05 02:36:02 +0100

    added some random data

--- server/run.py
+++ server/run.py
@@ -2,4 +2,29 @@
 
 import server
 
-server.run()
+# server.run()
+def runOp(run):
+    try:
+        run()
+    except Exception as e:
+        print(e)
+
+runOp(lambda :server.DB.insertCity("Cesena"))
+runOp(lambda :server.DB.insertCity("Asti"))
+runOp(lambda :server.DB.insertItem("Pane"))
+runOp(lambda :server.DB.insertItem("Pizza"))
+runOp(lambda :server.DB.insertItem("Briosche"))
+runOp(lambda :server.DB.insertItem("Spremuta"))
+runOp(lambda :server.DB.insertItem("caramelle"))
+runOp(lambda :server.DB.insertMethod("Contante"))
+runOp(lambda :server.DB.insertMethod("SanPaolo"))
+runOp(lambda :server.DB.insertMethod("Postepay"))
+runOp(lambda :server.DB.insertShop("Coop"))
+runOp(lambda :server.DB.insertShop("Paninaro"))
+runOp(lambda :server.DB.insertShop("Conad"))
+runOp(lambda :server.DB.insertShop("Luka's Pizza"))
+runOp(lambda :server.DB.insertPayment("-", "Cesena", "Coop", "Postepay"))
+runOp(lambda :server.DB.insertPayment("-", "Cesena", "Luka's Pizza", "Postepay"))
+runOp(lambda :server.DB.insertDetailOrder("Briosche", 1, 2, 12.00))
+runOp(lambda :server.DB.insertDetailOrder("Spremuta", 1, 1, 29.00))
+runOp(lambda :server.DB.insertDetailOrder("Pizza", 2, 1, 89.00))
