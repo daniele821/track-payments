e9c9d00e2acdf0a5b12a17f8d91ca55298f02aa5
Daniele Muffato <danixgithub1@gmail.com>
2025-04-17 17:56:01 +0200

    implemented Display for module error

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
 use crate::time::TimeParseError;
-use std::error;
+use std::{error, fmt::Display};
 
 #[derive(Debug)]
 pub enum Error {
@@ -18,3 +18,18 @@ impl From<Box<dyn error::Error>> for Error {
         Self::GenericError(value)
     }
 }
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Self::GenericError(Box::from(value))
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::TimeParseError(err) => write!(f, "{err}"),
+            Error::GenericError(err) => write!(f, "{err}"),
+        }
+    }
+}
+impl std::error::Error for Error {}
