9881554490b7456a3987778e41d74409d73e7433
Daniele Muffato <danixgithub1@gmail.com>
2025-04-17 17:24:33 +0200

    added time methods

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,17 @@
-use std::{error, result};
+use crate::time::TimeParseError;
+use std::error;
 
 #[derive(Debug)]
 pub enum Error {
+    TimeParseError(TimeParseError),
     GenericError(Box<dyn error::Error>),
 }
 
-pub type Result<T> = result::Result<T, Error>;
+impl From<TimeParseError> for Error {
+    fn from(value: TimeParseError) -> Self {
+        Self::TimeParseError(value)
+    }
+}
 
 impl From<Box<dyn error::Error>> for Error {
     fn from(value: Box<dyn error::Error>) -> Self {
--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,6 @@
-use chrono::{DateTime, Datelike, Local, TimeZone, Timelike, Utc};
+use std::{error::Error, fmt::Display};
+
+use chrono::{DateTime, Datelike, Local, NaiveDateTime, ParseError, TimeZone, Timelike, Utc};
 pub const DEFAULT_FORMAT: &str = "%Y/%m/%d %H:%M";
 
 #[derive(Debug)]
@@ -11,11 +13,30 @@ pub struct TimeFields {
     fields: DateTime<Utc>,
 }
 
+#[derive(Debug)]
+pub struct TimeParseError(ParseError);
+
 impl TimeStamp {
     pub fn now() -> Self {
         Self {
             unix_secs: Local::now().naive_local().and_utc().timestamp(),
         }
+    }
+
+    pub fn parse_str_fmt(str: &str, format: &str) -> Result<Self, TimeParseError> {
+        NaiveDateTime::parse_from_str(str, format)
+            .map(|time| Self::from(time.and_utc().timestamp()))
+            .map_err(TimeParseError)
+    }
+    pub fn parse_str(str: &str) -> Result<Self, TimeParseError> {
+        Self::parse_str_fmt(str, DEFAULT_FORMAT)
+    }
+
+    pub fn format_str_fmt(&self, format: &str) -> String {
+        TimeFields::from(self).fields.format(format).to_string()
+    }
+    pub fn format_str(&self) -> String {
+        self.format_str_fmt(DEFAULT_FORMAT)
     }
 }
 
@@ -49,18 +70,25 @@ impl From<i64> for TimeStamp {
     }
 }
 
-impl From<TimeFields> for TimeStamp {
-    fn from(value: TimeFields) -> Self {
+impl From<&TimeFields> for TimeStamp {
+    fn from(value: &TimeFields) -> Self {
         Self {
             unix_secs: value.fields.timestamp(),
         }
     }
 }
 
-impl From<TimeStamp> for TimeFields {
-    fn from(value: TimeStamp) -> Self {
+impl From<&TimeStamp> for TimeFields {
+    fn from(value: &TimeStamp) -> Self {
         Self {
             fields: DateTime::from_timestamp(value.unix_secs, 0).unwrap(),
         }
     }
 }
+
+impl Display for TimeParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl Error for TimeParseError {}
