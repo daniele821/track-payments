f040b52f8035768e10c794588b4a7241c4706eff
Daniele Muffato <danixgithub1@gmail.com>
2025-04-17 05:09:55 +0200

    wip: time

--- src/time.rs
+++ src/time.rs
@@ -1,22 +1,18 @@
-use chrono::Local;
+use chrono::{DateTime, Datelike, Local, Timelike, Utc};
 
 pub const DEFAULT_FORMAT: &str = "%Y/%m/%d %H:%M";
 
 #[derive(Debug)]
-pub struct DateTime {
+pub struct TimeStamp {
     unix_secs: i64,
 }
 
 #[derive(Debug)]
-pub struct DateTimeFields {
-    years: i32,
-    months: u8,
-    days: u8,
-    hours: u8,
-    minutes: u8,
+pub struct TimeFields {
+    fields: DateTime<Utc>,
 }
 
-impl DateTime {
+impl TimeStamp {
     pub fn now() -> Self {
         Self {
             unix_secs: Local::now().naive_local().and_utc().timestamp(),
@@ -24,42 +20,34 @@ impl DateTime {
     }
 }
 
-impl DateTimeFields {
-    pub fn new(years: i32, months: u8, days: u8, hours: u8, minutes: u8) -> Self {
-        Self {
-            years,
-            months,
-            days,
-            hours,
-            minutes,
-        }
+impl TimeFields {
+    pub fn years(&self) -> i32 {
+        self.fields.year()
     }
-
-    pub fn years(&self) -> &i32 {
-        &self.years
+    pub fn months(&self) -> u8 {
+        self.fields.month().try_into().unwrap()
     }
-    pub fn months(&self) -> &u8 {
-        &self.months
+    pub fn days(&self) -> u8 {
+        self.fields.day().try_into().unwrap()
     }
-    pub fn days(&self) -> &u8 {
-        &self.days
+    pub fn hours(&self) -> u8 {
+        self.fields.hour().try_into().unwrap()
     }
-    pub fn hours(&self) -> &u8 {
-        &self.hours
-    }
-    pub fn minutes(&self) -> &u8 {
-        &self.minutes
+    pub fn minutes(&self) -> u8 {
+        self.fields.minute().try_into().unwrap()
     }
 }
 
-impl From<i64> for DateTime {
+impl From<i64> for TimeStamp {
     fn from(value: i64) -> Self {
         Self { unix_secs: value }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    pub fn tet() {}
+impl From<TimeFields> for TimeStamp {
+    fn from(value: TimeFields) -> Self {
+        Self {
+            unix_secs: value.fields.timestamp(),
+        }
+    }
 }
