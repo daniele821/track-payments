3fa751e65ebabfe0ed42e18f372595dc7f659678
Daniele Muffato <danixgithub1@gmail.com>
2025-04-17 04:17:36 +0200

    time methods

--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,20 @@
+use chrono::Local;
+
 #[derive(Debug)]
 pub struct Time {
     unix_secs: i64,
+}
+
+impl Time {
+    pub fn now() -> Self {
+        Self {
+            unix_secs: Local::now().naive_local().and_utc().timestamp(),
+        }
+    }
+}
+
+impl From<i64> for Time {
+    fn from(value: i64) -> Self {
+        Self { unix_secs: value }
+    }
 }
